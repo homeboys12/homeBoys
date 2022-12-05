2e56c0c1dc0f011c3ef132b07038414c581d08ec
hyeunseungYu <hyeunseung03@gmail.com>
2022-12-05 23:02:16 +0900

    [update]점수산정 업데이트 > > 현재 크롤링 완료하였으며, 점수 산정 기능 완료가 필요함 > 아직 테스트는 진행하지 못한 상태

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 weatherData = requests.get(
@@ -11,16 +13,43 @@ weatherSoup = BeautifulSoup(weatherData.text, 'html.parser')
 weatherData_rain = weatherSoup.select_one(
     '#weather_table > tbody > tr:nth-child(42) > td:nth-child(9)').text
 weatherData_snow = weatherSoup.select_one(
-    '#weather_table > tbody > tr:nth-child(42) > td:nth-child(10)')
+    '#weather_table > tbody > tr:nth-child(42) > td:nth-child(10)').text
 
-print(weatherData_rain)
+weatherData_rain_result = ""
+weatherData_snow_result = ""
+weatherPoints_rain = 0
+weatherPoints_snow = 0
 
-for weatherList in weatherLists:
-    weatherList_result = weatherList.replace(" ", "강수예보가 없습니다.")
-    print(weatherList_result)
+if (weatherData_rain.isspace() == True):
+    weatherData_rain_result = "강수 확률이 없습니다."
+    weatherPoints_rain = 40
+else:
+    weatherData_rain_result = weatherData_rain
+    weatherPoints_rain = -30
 
 
-# @app.route("/article", methods=["GET"])
-# def article_get():
+if (weatherData_snow.isspace() == True):
+    weatherData_snow_result = "강설 확률이 없습니다."
+    weatherPoints_snow = 0
+else:
+    weatherData_snow_result = weatherData_snow
+    weatherPoints_snow = 40
 
-#     return jsonify({'lists': allArticleList})
+weatherData_list = [weatherData_rain_result, weatherData_snow_result]
+weatherPoints_list = [weatherPoints_rain, weatherPoints_snow]
+
+print(weatherPoints_snow, weatherPoints_rain)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/weather", methods=["GET"])
+def weather_get():
+    return jsonify({'weatherLists': weatherData_list, 'weatherpoints': weatherPoints_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
