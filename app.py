74b6acc69338843995a0dd4c9230cc7541633890
Hyeunseung <hyeunseung03@gmail.com>
2022-12-06 10:16:54 +0900

    [fix]강수량, 적설량 표기
    강수량, 적설량 데이터도 표기할 수 있도록 하였음.

--- app.py
+++ app.py
@@ -11,22 +11,26 @@ weatherData = requests.get(
 
 weatherSoup = BeautifulSoup(weatherData.text, 'html.parser')
 
+# 강수, 적설 크롤링
 weatherData_rain = weatherSoup.select_one(
     '#weather_table > tbody > tr:nth-child(42) > td:nth-child(9)').text
 weatherData_snow = weatherSoup.select_one(
     '#weather_table > tbody > tr:nth-child(42) > td:nth-child(10)').text
 
-
+# 기상상태 메시지 입력을 위한 변수 생성
 weatherData_rain_msg = ""
 weatherData_snow_msg = ""
 
+# 기상상태 점수 입력을 위한 변수 생성
 weatherPoints_rain = 0
 weatherPoints_snow = 0
 
+# 현재 강수, 적설량 표현을 위한 변수 생성
 weatherData_result_rain = 0
 weatherData_result_snow = 0
 
 
+# 현재 강수, 적설 상태 확인
 if (weatherData_rain.isspace() == True):
     weatherData_rain_msg = "비 예보가없습니다."
     weatherPoints_rain = 40
@@ -56,11 +60,12 @@ else:
     weatherData_snow_result = weatherData_snow
     weatherPoints_snow = 40
 
-
+# 현재 강수, 적설 데이터를 가지는 리스트 생성
 weatherMsg_list = [weatherData_rain_msg, weatherData_snow_msg]
 weatherPoints_list = [weatherPoints_rain, weatherPoints_snow]
-weatherResult_list = [weatherData_rain, weatherData_snow]
+weatherResult_list = [float(weatherData_rain), float(weatherData_snow)]
 
+# 잘 나오는 지 확인
 print(weatherPoints_snow, weatherPoints_rain)
 print(weatherData_rain_msg)
 print(weatherResult_list)
@@ -73,8 +78,10 @@ def home():
 
 @app.route("/weather", methods=["GET"])
 def weather_get():
-    return jsonify({'weatherMsgLists': weatherMsg_list, 'weatherpoints': weatherPoints_list})
+    return jsonify({'weatherMsgLists': weatherMsg_list,
+                    'weatherPoints': weatherPoints_list,
+                    'weatherResult': weatherResult_list})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
